5a988e7143e4fcf04a8ac0da0e63c916c94803f5
jorgewattes <jorgelwoj@hotmail.com>
2022-04-04 15:17:32 -0300

    N0-IFCE Guaramiranga

---
+++ EleBas_N0.py
@@ -0,0 +1,62 @@
+import numpy as np
+import math
+
+def Q1():
+    V=110*np.random.randint(1,3)
+    P=np.random.randint(2,6)*1200
+    enun=f'Um chuveiro elétrico feito para instalações de {V:.0f}V, e com uma potência de {P:.0f} foi instalado em um circuito com disjuntor de 20A. Buscando saber se o circuito era adequado ao equipamento, calculou-se a corrente do mesmo, qual o valor obtido? o circuito é adequado?\nResposta:__________'
+    gab=f'{P/V:.3f}A, {"Sim" if P/V<20 else "não"}'
+    return[enun,gab]
+
+def Q2():
+    I=1*np.random.randint(1,5)
+    Cores=['Preto','Marrom','Vermelho','Laranja','Amarelo','Verde','Azul','Violeta','Cinza','Branco']
+    c1=np.random.randint(1,10)
+    c2=np.random.randint(1,10)
+    c3=np.random.randint(1,5)
+    R=int(f'{c1}{c2}') *(10**c3)
+    Cor1=Cores[c1]
+    Cor2=Cores[c2]
+    Cor3=Cores[c3]
+
+    enun=f'Um resistor com as seguintes cores, terá uma resistência de quantos ohms?\n{Cor1} | {Cor2} | {Cor3} | Dourado\nResposta:__________'
+    gab=f'{R} Ohms'
+    return[enun,gab]
+
+def Q3():
+    V=6*np.random.randint(1,4)
+    R=np.random.randint(12,67)*10
+    P=(V**2)/R
+    enun=f'Alimentando com uma fonte de {V:.0f} V, um resistor de {R:.0f} Ohms, ele irá aquecer, transformando energia elétrica em térmica com que potência?\nResposta:__________'
+    gab=f'{P/V:.3f} W'
+    return[enun,gab]
+
+def Q4():
+    V=5*np.random.randint(1,5)
+    n=np.random.randint(2,5)
+    R=np.random.randint(12,67)*10
+    Req=R/n
+    P=(V**2)/Req
+    enun=f'Alimentando com uma fonte de {V:.0f} V, {n:.0f} resistores de {R:.0f} Ohms são conectados em paralelo. Qual a potência total consumida pelos resistores?\nResposta:__________'
+    gab=f'{P/V:.3f} W'
+    return[enun,gab]
+
+def Q5():
+    V=110*np.random.randint(1,3)
+    P=np.random.randint(2,6)*1200
+    R1=(V**2)/P
+    R2=(V**2)/(P/2)
+    enun=f'Um chuveiro elétrico de {P:.0f} W feito para instalações de {V:.0f} V, possui um seletor de inverno e verão. Na opção verão, o consumo cai pela metade. Qual o valor da resistência na opção inverno? e na opção verão? Resposta:__________'
+    gab=f'Inverno: {R1:.2f} Ohms, Verão: {R2:.2f} Ohms'
+    return[enun,gab]
+
+def Q6():
+    n=np.random.randint(8,20)
+    tar=np.random.randint(74,86)/100
+    h=np.random.randint(7,10)
+    P=np.random.randint(10,14)*100
+    E=P*n*h/1000
+    val=E*tar
+    enun=f'Um ar-condicionado de 7.500 Btus, tem em média {P:.0f} W. Um casal que ligue o ar-condicionado apenas em noites quentes, observou que em um mês de 31 dias, ligou o ar-condicionado apenas {n:.0f} vezes. Considerando a tarifa de {tar:.2f} R$/kW.h, o uso noturno de {h:.0f} horas. Qual será o valor, em reais, da conta de energia referente ao uso do ar-condicionado?\nResposta:__________'
+    gab=f'R$ {val:.2f} '
+    return[enun,gab]
---
+++ N0_MSI_EleBas.py
@@ -0,0 +1,83 @@
+import numpy as np
+import math
+import EleBas_N1 as EB
+
+alunos=[
+'Adriano Pires Ferreira',
+'Ana Alice Araujo Nogueira',
+'Camile Jordana Franco Garcia',
+'Camilly Vitória Gomes De Sousa',
+'Clenia Santos Silva Ferreira',
+'Daniel Ferreira Simões',
+'Derlan Da Silva Oliveira',
+'Felipe Soares Gomes',
+'Francisca Maria Queiroz Da Silva',
+'Francisco Ivan Gomes De Oliveira',
+'Gustavo Chagas Barbosa',
+'Hygo De Souza E Silva',
+'João Victor Rocha Costa',
+'José Wellington Nunes Pereira',
+'Kailany Santos Vieira',
+'Leandro Oliveira De Lima',
+'Lohanne Da Silva Vieira',
+'Lucas Vinicius De Oliveira Lima',
+'Marcelo De Oliveira Melo Costa',
+'Maria Da Glória De Cristo Félix',
+'Paulo Roberto Nepomuceno De Sousa',
+'Pedro Evander Lopes Da Silva',
+'Pedro Ryan Ferreira Lima',
+'Rafael Luz Gomes',
+'Raquel Das Chagas',
+'Rogerson Sousa Do Nascimento',
+'Sabrina Rodrigues De Amorim',
+'Tamires Vitória Silva De França',
+'Wagner Gomes De Oliveira'
+]
+
+resp={}
+enunciado=''
+gabaritoaluno=''
+gabarito={}
+
+
+# Questoes
+for x in range(len(alunos)):
+    #print(alunos[x])
+    enunciado=enunciado+'Prova N0! (0-pontos)\nAluno: '+ alunos[x]+' \n'
+
+    [enun,gab]=EB.Q1()
+    gabaritoaluno=gabaritoaluno+'Q1)'+gab+' / '
+    enunciado=enunciado+f'{enun}\n\n\n\n\n'
+
+    [enun,gab]=EB.Q2()
+    gabaritoaluno=gabaritoaluno+'Q2)'+gab+' / '
+    enunciado=enunciado+f'{enun}\n\n\n\n\n'
+
+    [enun,gab]=EB.Q3()
+    gabaritoaluno=gabaritoaluno+'Q3)'+gab+' / '
+    enunciado=enunciado+f'{enun}\n\n\n\n\n'
+
+    [enun,gab]=EB.Q4()
+    gabaritoaluno=gabaritoaluno+'Q4)'+gab+' / '
+    enunciado=enunciado+f'{enun}\n\n\n\n\n'
+
+    [enun,gab]=EB.Q5()
+    gabaritoaluno=gabaritoaluno+'Q5)'+gab+' / '
+    enunciado=enunciado+f'{enun}\n\n\n\n'
+
+    [enun,gab]=EB.Q6()
+    gabaritoaluno=gabaritoaluno+'Q6)'+gab
+    enunciado=enunciado+f'{enun}\n'
+
+    gabarito[alunos[x]]=gabaritoaluno
+    gabaritoaluno=''
+#Gabarito
+enunciado=enunciado+f'Gabarito Prova N2\n'
+for x in range(len(alunos)):
+    enunciado=enunciado+f'{alunos[x]}\n{gabarito[alunos[x]]}\n'
+
+#enunciado=enunciado.replace('.',',')
+print(enunciado)
+#file = open('RegraDeTes.txt','w+')
+#file.write(enunciado)
+#file.close()
